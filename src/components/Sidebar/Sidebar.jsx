fc2b8c3a1a1841b7952c4372ce28e9bc70c0bb1c
Atul Tingre <atultingre.work@gmail.com>
2024-04-24 15:50:04 +0530

    sidebar and navbar added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,25 @@
-import React from "react";
-import "./Sidebar.scss"
+import "./Sidebar.scss";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List Item</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>Orders</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
